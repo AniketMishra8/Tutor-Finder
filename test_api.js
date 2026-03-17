b7eaf57ea772d000cd52100c794bd356639017b3
Aniket Mishra <mishraaniket302@gmail.com>
2026-03-17 11:44:08 +0530

    Managing server

--- test_api.js
+++ test_api.js
@@ -1,7 +1,15 @@
 import fs from 'fs';
-const url = "https://generativelanguage.googleapis.com/v1beta/models?key=AIzaSyCac9Ck6qx6W4x0ZZel535k-05rfXQo_u0";
+import dotenv from 'dotenv';
+import process from 'process';
+dotenv.config();
+
+const apiKey = process.env.VITE_GEMINI_API_KEY;
+const url = `https://generativelanguage.googleapis.com/v1beta/models?key=${apiKey}`;
 
 fetch(url)
   .then(res => res.json())
-  .then(data => fs.writeFileSync('models_response.json', JSON.stringify(data, null, 2)))
-  .catch(err => console.error(err));
+  .then(data => {
+    fs.writeFileSync('models_response.json', JSON.stringify(data, null, 2));
+    console.log('API response saved to models_response.json');
+  })
+  .catch(err => console.error('Error fetching models:', err));
